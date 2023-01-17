e506e5e5367becd6e9f7fba64720e09768df9e3d
Yash Patel <86044919+Yash472001@users.noreply.github.com>
2023-01-17 18:00:55 +0530

    Added with latest changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
+import DeleteTodo from './components/DeleteTodo';
 
 function App() {
   return (
-    <div className="App">
-      <AddTodo/>
-      <TodoList/>
+    <div className="App" style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh",flexDirection: "column" }}>
+      <div>
+        <AddTodo />
+      </div>
+      <div>
+        <TodoList />
+      </div>
+      <div > 
+        <DeleteTodo />
+      </div>
+
     </div>
   );
 }
